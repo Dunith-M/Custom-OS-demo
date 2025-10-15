76b232fd6a146a8984ce60d29ad654f596a40bb1
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-10-15 20:39:46 +0530

    update

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,9 +4,14 @@
 section code
 
 .init:
+    mov eax, 0x07c0
+    mov ds, eax
     mov eax, 0xb800
     mov es, eax
     mov eax, 0 ; set eax to 0 -> i = 0
+    mov ebx, 0 ; Index of the character in the string , we printing
+    mov ecx, 0 ; Actual adress of the character
+    mov dl, 0 ; Actual value that we are printing 
 
 
 .clear:
@@ -19,13 +24,31 @@ section code
 
     jl .clear
 
+mov eax, text1
+push .end
 
-.main:
-    mov byte [es:0x00], 'H' ; 0xb800 + 0x00 = 0xb800
-    mov byte [es:0x01], 0x30
+.print:
+    mov dl, byte [eax + ebx]
 
+    cmp dl, 0
+    je .print_end
 
-jmp $
+    mov byte [es:ecx], dl
+
+    inc ebx
+    inc ecx        
+    inc ecx
+
+    jmp .print
+
+.print_end:
+    mov eax, 0;
+
+.end:
+    jmp $
+
+text: db 'Hello World!', 0
+text1: db 'Vidusahan, Heshani, Malanka, Themiya, Dunith', 0
 
 times 510 - ($ - $$) db 0x00 ; Pads the files with 0s, making it the right size
 
