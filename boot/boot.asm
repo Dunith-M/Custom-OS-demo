e99f94b24c290546d0530437385d322eef022808
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-10-15 20:23:03 +0530

    clear screen and change the the full bckground

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,12 +3,27 @@
 
 section code
 
-.main:
+.init:
     mov eax, 0xb800
     mov es, eax
+    mov eax, 0 ; set eax to 0 -> i = 0
 
+
+.clear:
+    mov byte [es:eax], 0 ; Move blank character to current address
+    inc eax
+    mov byte [es:eax], 0x30 ; Move the bkground color and character color to next address
+    inc eax
+
+    cmp eax, 2 * 25 * 80
+
+    jl .clear
+
+
+.main:
     mov byte [es:0x00], 'H' ; 0xb800 + 0x00 = 0xb800
     mov byte [es:0x01], 0x30
+
 
 jmp $
 
