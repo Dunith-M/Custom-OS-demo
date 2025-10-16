33f0a1eb4a68799517d876a36b4225621f354b0f
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-10-16 22:51:11 +0530

    small update with extended comments

--- boot/boot.asm
+++ boot/boot.asm
@@ -9,48 +9,49 @@ section code
     mov eax, 0xb800
     mov es, eax
     mov eax, 0 ; set eax to 0 -> i = 0
-    mov ebx, 0 ; Index of the character in the string , we printing
-    mov ecx, 0 ; Actual adress of the character
-    mov dl, 0 ; Actual value that we are printing 
-
+    mov ebx, 0 ; Index of the character in the string that we are printing
+    mov ecx, 0 ; Actual address of the chracter on the screen memory
+    mov dl, 0 ; Actual value of the character that we are printing
 
 .clear:
-    mov byte [es:eax], 0 ; Move blank character to current address
+    mov byte [es:eax], 0
     inc eax
-    mov byte [es:eax], 0x30 ; Move the bkground color and character color to next address
+    mov byte [es:eax], 0x30
     inc eax
 
     cmp eax, 2 * 25 * 80
 
     jl .clear
 
-mov eax, text1
-push .end
-
-.print:
-    mov dl, byte [eax + ebx]
-
-    cmp dl, 0
-    je .print_end
-
-    mov byte [es:ecx], dl
-
-    inc ebx
-    inc ecx        
-    inc ecx
-
-    jmp .print
-
-.print_end:
-    mov eax, 0;
+mov eax, text2
+call .print
+;call .print
 
 .end:
     jmp $
 
-text: db 'Hello World!', 0
-text1: db 'Vidusahan, Heshani, Malanka, Themiya, Dunith', 0
+.print:
+    mov dl, byte [eax + ebx]
+    
+    cmp dl, 0
+    je .print_end
+    
+    mov byte [es:ecx], dl
 
-times 510 - ($ - $$) db 0x00 ; Pads the files with 0s, making it the right size
+    inc ebx
+    inc ecx
+    inc ecx
+    
+    jmp .print
+
+.print_end:
+    ret
+
+text: db 'Hello, World!,', 0
+text1: db 'This is another text', 0
+text2: db 'Vidusahan , Heshani, Malanka, Themiya, Dunith', 0
+
+times 510 - ($ - $$) db 0x00 ; Pads the file with 0s, making the file the right size
 
 db 0x55
 db 0xaa
